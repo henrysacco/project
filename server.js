830d716a458cfe202ba7f82bd70ea712ff9f6e1f
henrysacco <150164685+henrysacco@users.noreply.github.com>
2024-04-24 11:59:39 -0400

    completed stage02 add getCustomer

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // server.js file
 const express = require("express");
+const da = require("./data-access");
 const path = require("path"); // for handling file paths
 
 const app = express();
@@ -10,4 +11,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+});
+
+app.get("/customers", async (req, res) => {
+  const cust = await da.getCustomers();
+  res.send(cust);
 });
